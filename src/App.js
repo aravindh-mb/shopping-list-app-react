cfcdee3b188ce8bc2f1a2dd4c5349ade1a31e9d8
Aravindh <83901906+aravindh-mb@users.noreply.github.com>
2022-07-30 00:30:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ShoppingList  from './ShoppingLocalStorage'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ShoppingList></ShoppingList>
+      </header>
+    </div>
+  );
+}
+
+export default App;
